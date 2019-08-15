5ca7b2ee7b222918929d5f33fd1edc821dc0d61b
Hinageshi <marie.bouin@protonmail.com>
2019-08-15 16:45:44 +0200

    * Added new font (Comix-Loud) * Added image assets * Made HomePage UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_picolo_clone/view/home_page.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIOverlays([]);
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Picolo Clone',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -15,4 +18,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -7,13 +7,79 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
+//TODO : change colors (find a nice color scheme)
+
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.indigo,
       body: Center(
-        child: Center(
-          child: Text("Hey, that's a beginning !"),
+        child: Stack(
+          children: <Widget>[
+            Align(
+              alignment: Alignment.topRight,
+              child: Padding(
+                padding: EdgeInsets.all(12.0),
+                child: Icon(
+                  Icons.settings,
+                  size: 35.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 150.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    "BIB'INSA",
+                    style: TextStyle(
+                        fontFamily: 'Comix-Loud',
+                        color: Colors.white,
+                        fontSize: 35.0),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+            ),
+            Align(
+                alignment: Alignment.bottomCenter,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Container(
+                      height: 190.0,
+                      child: Image.asset("assets/images/race.png"),
+                    ),
+                    Container(
+                      height: 170.0,
+                      color: Colors.blue[300],
+                      child: Center(
+                        child: RaisedButton(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: new BorderRadius.circular(20.0)),
+                          color: Colors.white,
+                          onPressed: () {},
+                          child: Padding(
+                            padding:
+                                EdgeInsets.fromLTRB(20.0, 30.0, 20.0, 20.0),
+                            child: Text(
+                              "PLAY",
+                              style: TextStyle(
+                                  fontFamily: 'Comix-Loud',
+                                  color: Colors.blue[300],
+                                  fontSize: 30.0),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ))
+          ],
         ),
       ),
     );
